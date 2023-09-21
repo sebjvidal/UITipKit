38c287e9f949285d40e9353925e4ec6222862c88
sebjvidal <seb.vidal@btinternet.com>
2023-09-21 18:20:16 +0100

    Update `.gitignore`

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "UITipKit",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UITipKit",
-            targets: ["UITipKit"]),
+            targets: ["UITipKit"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UITipKit"),
-        .testTarget(
-            name: "UITipKitTests",
-            dependencies: ["UITipKit"]),
+            name: "UITipKit"
+        ),
     ]
 )
