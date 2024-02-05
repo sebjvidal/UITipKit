c098e45388d46716c6b949937b25494fbc13439a
sebjvidal <seb.vidal@btinternet.com>
2024-02-05 15:30:01 +0000

    Add `Configuration` typealias in `UITipTableViewCell` extension

--- Sources/UITipKit/Base/UITipTableViewCell/UITipTableViewCell+Configuration.swift
+++ Sources/UITipKit/Base/UITipTableViewCell/UITipTableViewCell+Configuration.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
-//  
+//  UITipTableViewCell+Configuration.swift
+//
 //
 //  Created by Seb Vidal on 05/02/2024.
 //
 
 import Foundation
+
+extension UITipTableViewCell {
+    typealias Configuration = UITipView.Configuration
+}
