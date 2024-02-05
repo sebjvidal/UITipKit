9f0e02fd03e13e016d1d4a6095086b6b784cb08e
sebjvidal <seb.vidal@btinternet.com>
2024-02-05 15:11:22 +0000

    Cleanup `UITipView.Configuration.ImageProperties`

---
+++ Sources/UITipKit/Base/UITipView/UITipView+Configuration+ImageProperties.swift
@@ -0,0 +1,18 @@
+//
+//  UITipView+Configuration+ImageProperties.swift
+//
+//
+//  Created by Seb Vidal on 05/02/2024.
+//
+
+import UIKit
+
+public extension UITipView.Configuration {
+    struct ImageProperties {
+        // MARK: - Public Properties
+        public var tintColor: UIColor? = nil
+        
+        // MARK: - init()
+        internal init() {}
+    }
+}
