feb6a05126b1fa4dfa549a11cfcf4bfc1bdb7c95
sebjvidal <seb.vidal@btinternet.com>
2024-02-05 15:11:00 +0000

    Add `CloseButton` struct in extension of `UITipView.Configuration`

---
+++ Sources/UITipKit/Base/UITipView/UITipView+Configuration+CloseButton.swift
@@ -0,0 +1,18 @@
+//
+//  UITipView+Configuration+CloseButton.swift
+//
+//
+//  Created by Seb Vidal on 05/02/2024.
+//
+
+import Foundation
+
+public extension UITipView.Configuration {
+    struct CloseButton {
+        // MARK: - Public Properties
+        public var isHidden: Bool = false
+        
+        // MARK: - init()
+        internal init() {}
+    }
+}
