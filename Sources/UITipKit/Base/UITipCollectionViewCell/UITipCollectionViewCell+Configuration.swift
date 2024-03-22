3096addf3569e1a26b24e474c07f050f0d5e9e1a
sebjvidal <seb.vidal@btinternet.com>
2024-03-22 09:12:09 +0000

    Add initial `UITipCollectionViewCell`

---
+++ Sources/UITipKit/Base/UITipCollectionViewCell/UITipCollectionViewCell+Configuration.swift
@@ -0,0 +1,12 @@
+//
+//  UITipCollectionViewCell+Configuration.swift
+//
+//
+//  Created by Seb Vidal on 22/03/2024.
+//
+
+import UIKit
+
+extension UITipCollectionViewCell {
+    typealias Configuration = UITipView.Configuration
+}
---
+++ Sources/UITipKit/Base/UITipCollectionViewCell/UITipCollectionViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  UITipCollectionViewCell.swift
+//
+//
+//  Created by Seb Vidal on 22/03/2024.
+//
+
+import UIKit
+
+public class UITipCollectionViewCell: UICollectionViewCell {
+    // MARK: - Private Properties
+    private var tipView: UITipView!
+    
+    // MARK: - Public Properties
+    public var configuration: UITipView.Configuration? {
+        get {
+            return tipView.configuration
+        } set {
+            tipView.configuration = newValue
+            contentView.invalidateIntrinsicContentSize()
+        }
+    }
+    
+    // MARK: - init(frame:)
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCollectionViewCell()
+        setupTipView()
+    }
+    
+    // MARK: - init(configuration:)
+    convenience init(configuration: UITipView.Configuration) {
+        self.init(frame: .zero)
+        self.configuration = configuration
+    }
+    
+    // MARK: - init(coder:)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupCollectionViewCell() {
+        backgroundColor = .clear
+    }
+    
+    private func setupTipView() {
+        tipView = UITipView()
+        tipView.configuration = UITipView.Configuration()
+        tipView.configuration?.background.cornerRadius = 0
+        tipView.configuration?.background.backgroundColor = .clear
+        tipView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(tipView)
+        
+        NSLayoutConstraint.activate([
+            tipView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            tipView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            tipView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            tipView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - layoutSubviews()
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.invalidateIntrinsicContentSize()
+    }
+}
